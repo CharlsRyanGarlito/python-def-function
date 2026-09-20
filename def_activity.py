89fe2f6603fcfc798267547097e827465c094425
CharlsRyanGarlito <yanderezizi6@gmail.com>
2026-09-21 00:40:52 +0800

    Create def_activity.py

---
+++ def_activity.py
@@ -0,0 +1,7 @@
+# This def function is used to create a reusable block of code that performs a specific task.
+
+def greet_user(name):
+    print("Hello, " + name + "! Welcome to Python.")
+
+# Calling/invoking the function
+greet_user("Student")
